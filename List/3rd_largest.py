c3aa7043d595dd1e11496c01f0ede5daa16ac9fc
Skaria Mathew <145376481+smstyles@users.noreply.github.com>
2025-02-02 15:06:15 +0530

    Delete List directory

--- List/3rd_largest.py
+++
@@ -1,9 +0,0 @@
-
-a=[]
-n=int(input("Enter the number of elements in the list:"))
-for i in range(0,n):
-    ele=int(input("Enter the element:"))
-    a.append(ele)
-print(a)
-a.sort()
-print(a[-3])
